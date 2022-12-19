3f23460c26cf45a2546a8181698f6e64fb06abb2
Jonathan Ortigueras <ortiguerasjonathan@gmail.com>
2022-12-19 19:48:58 +0800

    Updates

--- static/uztwt4YpELdqx_FEzDk8a/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-b7c1fd7c3b1066b5.js","static/chunks/pages/index-1b4588a1bc07db00.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
